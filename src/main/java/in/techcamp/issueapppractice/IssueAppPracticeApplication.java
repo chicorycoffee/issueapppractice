8947870c9063850721f1bb35d3b3e44ef9cb304b
chicorycoffee <yk.nonogaki@gmail.com>
2023-06-24 22:13:36 +0900

    first commit

---
+++ src/main/java/in/techcamp/issueapppractice/IssueAppPracticeApplication.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueAppPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueAppPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/techcamp/issueapppractice/IssueAppPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapppractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueAppPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
