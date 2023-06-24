1b058056a51be47d91206ad34b0ebf29a81c5c31
chicorycoffee <yk.nonogaki@gmail.com>
2023-06-24 23:48:45 +0900

    削除機能の実装

--- src/main/java/in/techcamp/issueapppractice/IssueRepository.java
+++ src/main/java/in/techcamp/issueapppractice/IssueRepository.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.Delete;
 
 import java.util.List;
 
@@ -19,5 +20,8 @@ public interface IssueRepository {
 
     @Update("UPDATE issues SET title = #{title}, content = #{content}, period = #{period}, importance = #{importance} WHERE id =#{id}")
     void update(long id, String title, String content, String period, String importance);
+
+    @Delete("delete from issues where id=#{id}")
+    void deleteById(Long id);
 }
 
